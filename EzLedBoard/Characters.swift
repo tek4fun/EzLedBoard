2ce369476adba3249bcc09e9156f6c8257e927f2
techmaster <techmaster@techmasters-iMac.local>
2017-03-28 15:05:53 +0700

    first Commit

---
+++ EzLedBoard/Characters.swift
@@ -0,0 +1,23 @@
+//
+//  Characters.swift
+//  EzLedBoard
+//
+//  Created by techmaster on 3/28/17.
+//  Copyright © 2017 techmaster. All rights reserved.
+//
+
+import UIKit
+class Characters {
+    let A = [[0,1,1,0],
+             [1,0,0,1],
+             [1,0,0,1],
+             [1,1,1,1],
+             [1,0,0,1],
+             [1,0,0,1]]
+    let B = [[0,1,1,0],
+             [1,0,0,1],
+             [1,0,0,1],
+             [1,1,1,1],
+             [1,0,0,1],
+             [1,0,0,1]]
+}
