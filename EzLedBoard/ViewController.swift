a7abecc9672e7de78491ea8f0bbd81c60163d4ae
techmaster <techmaster@techmasters-iMac.local>
2017-03-28 14:53:38 +0700

    Initial Commit

---
+++ EzLedBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EzLedBoard
+//
+//  Created by techmaster on 3/28/17.
+//  Copyright © 2017 techmaster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
