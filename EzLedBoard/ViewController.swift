7d33a5a7a929152c857797a5c40c4ce0ba1bbf07
techmaster <techmaster@techmasters-iMac.local>
2017-03-28 17:20:04 +0700

    Commit 2

--- EzLedBoard/ViewController.swift
+++ EzLedBoard/ViewController.swift
@@ -9,17 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let A = [[0,1,1,0],
+             [1,0,0,1],
+             [1,0,0,1],
+             [1,1,1,1],
+             [1,0,0,1],
+             [1,0,0,1]]
+    let B = [[1,1,1,0],
+             [1,0,0,1],
+             [1,1,1,0],
+             [1,0,0,1],
+             [1,0,0,1],
+             [1,1,1,0]]
+    var count = 0
+    var viewBoard = [[Int]]()
+    var move = Timer()
+    var preTemp = [[Int]](repeating:[Int](repeating:0,count:13),count:6)
+    var currentTemp = [[Int]](repeating:[Int](repeating:0,count:13),count:6)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        viewBoard = [[Int]](repeating:[Int](repeating:0,count:13),count:6)
+        //viewBoard[3][2] = 1
+        
+        //move = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(printthis), userInfo: nil, repeats: true)
+        printthis(chracter: A)
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func printthis(chracter:[[Int]]) {
+        
+        for index in 0...9 - count * 4{
+            for iRow in 0...5{
+                for iCol in 0...3{
+                    viewBoard[iRow][iCol + index] = chracter[iRow][iCol]
+                }
+            }
+            //print(self.viewBoard)
+            currentTemp = viewBoard
+            viewBoard = preTemp
+        }
+        count += 1
+        print(count)
+        preTemp = currentTemp
+        //viewBoard = currentTemp
+        print(viewBoard)
+        if count > 2 {
+            print("End!")
+        }
+        
+        printthis(chracter: B)
+        //move.invalidate()
     }
-
-
+    
+    
+    
+    
 }
 
